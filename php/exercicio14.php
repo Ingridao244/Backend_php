fe98567ff97d4f2d784116833ae09b1e8dcfd03d
Ingridao244 <ingrid.sepulvedra.senai404@gmail.com>
2024-09-18 15:19:12 -0300

    Add files via upload

---
+++ php/exercicio14.php
@@ -0,0 +1,30 @@
+<?php
+
+//crie um array mult.
+$usuario = [
+[ "Nome" => "Ingrid Sepulvedra",
+"Cidade" => "Cerquilho",
+"Estado" => "SP"],
+
+[ "Nome" => "Pedro Henrique",
+"Cidade" => "Itaquera",
+"Estado" => "SP"],
+
+[ "Nome" => "Emenuela luz",
+"Cidade" => "Recife",
+"Estado" => "PE"],
+
+[ "Nome" => "Daniel Oliveira",
+"Cidade" => "Xique-Xique",
+"Estado" => "BA"],
+];
+
+//Define a variavel
+$busca = "Pedro Henrique";
+
+//exibe a estrutura da array
+echo'<pre>'
+print_r($usuario);
+echo'</pre>;
+
+?>
